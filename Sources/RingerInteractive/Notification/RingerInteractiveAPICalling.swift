644f1abd07d6c80fe9ee071f61bb601dfb5e0759
anshul_ebiz <anshul.shah@esparkbizmail.com>
2021-11-01 15:02:45 +0530

    auth change

--- Sources/RingerInteractive/Notification/RingerInteractiveAPICalling.swift
+++ Sources/RingerInteractive/Notification/RingerInteractiveAPICalling.swift
@@ -5,7 +5,7 @@ extension RingerInteractiveNotification {
     public func ringerInteractiveLogin(username: String, password: String) {
         var header: [String : String] = [:]
         header["Content-Type"] = "application/json"
-        header["Auth"] = "{\"Username\":\"\(username)\",\"Password\":\"\(password)\"}"
+        header["auth"] = "{\"Username\":\"\(username)\",\"Password\":\"\(password)\"}"
         
         let boundary = WebAPIManager().generateBoundary()
         
