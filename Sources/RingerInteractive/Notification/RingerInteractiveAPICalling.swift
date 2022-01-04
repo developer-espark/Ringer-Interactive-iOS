99a630f92cfb785f3e75deb9d6a788f5bdd1ab07
developer-espark <jaykishan@esparkinfo.com>
2022-01-04 19:28:19 +0530

    totalcount change

--- Sources/RingerInteractive/Notification/RingerInteractiveAPICalling.swift
+++ Sources/RingerInteractive/Notification/RingerInteractiveAPICalling.swift
@@ -118,6 +118,8 @@ extension RingerInteractiveNotification {
                 self.group.enter()
                 ContactSave().downloadImageAndContactSave(name: contactListModel.objects[index].firstName + " " + contactListModel.objects[index].lastName, number: contactListModel.objects[index].phone, editNumber: contacts, imageUrl: contactListModel.objects[index].imageUrl)
             }
+        } else {
+            totalCount = 0
         }
     }
 }
