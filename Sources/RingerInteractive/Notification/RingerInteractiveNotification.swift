c40a9e82132894a2807051f886a307cbbe26adef
anshul_ebiz <anshul.shah@esparkbizmail.com>
2022-02-15 19:47:44 +0530

    new changes

--- Sources/RingerInteractive/Notification/RingerInteractiveNotification.swift
+++ Sources/RingerInteractive/Notification/RingerInteractiveNotification.swift
@@ -37,9 +37,9 @@ extension RingerInteractiveNotification {
         registerForRemoteNotifications()
         let contact = ContactSave()
         contact.requestAccess()
-        CoreDataManager().insertContact(contact: LocalContactModel(contactId: "123456789", createdAt: "02/02/22", createdBy: "02/02/22", deletedAt: "02/02/22", firstName: "test", galleryId: "1234567890", id: "1", lastName: "Testing", modifiedAt: "123456987", modifiedBy: "321645", contactImage: Data()))
+        _ = CoreDataManager().insertContact(contact: LocalContactModel(contactId: "123456789", createdAt: "02/02/22", createdBy: "02/02/22", deletedAt: "02/02/22", firstName: "test", galleryId: "1234567890", id: "1", lastName: "Testing", modifiedAt: "123456987", modifiedBy: "321645", contactImage: Data()))
         let data = CoreDataManager().fetchAllContact()
-        print(data)
+        print(data as Any)
     }
     
     func registerForRemoteNotifications() {
