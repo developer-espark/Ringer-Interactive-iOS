51307cbce76d1de6b60d05090a61e1f140f6028a
anshul_ebiz <anshul.shah@esparkbizmail.com>
2022-02-28 15:27:17 +0530

    old code

--- Sources/RingerInteractive/Contact/ContactSave.swift
+++ Sources/RingerInteractive/Contact/ContactSave.swift
@@ -254,61 +254,32 @@ public class ContactSave {
         return results
     }
 }
-//extension UIImage {
-//    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
-//        // Determine the scale factor that preserves aspect ratio
-//        let widthRatio = targetSize.width / size.width
-//        let heightRatio = targetSize.height / size.height
-//
-//        let scaleFactor = min(widthRatio, heightRatio)
-//
-//        // Compute the new image size that preserves aspect ratio
-//        let scaledImageSize = CGSize(
-//          width: size.width * scaleFactor,
-//          height: size.height * scaleFactor
-//        )
-//
-//        // Draw and return the resized UIImage
-//        let renderer = UIGraphicsImageRenderer(
-//          size: scaledImageSize
-//        )
-//
-//        let scaledImage = renderer.image { _ in
-//            self.draw(in: CGRect(
-//              origin: .zero,
-//              size: scaledImageSize
-//            ))
-//        }
-//
-//        return scaledImage
-//    }
-//}
-
 extension UIImage {
-
     func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
-        let size = self.size
-
-        let widthRatio  = targetSize.width  / size.width
+        // Determine the scale factor that preserves aspect ratio
+        let widthRatio = targetSize.width / size.width
         let heightRatio = targetSize.height / size.height
-
-        // Figure out what our orientation is, and use that to form the rectangle
-        var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        
+        let scaleFactor = min(widthRatio, heightRatio)
+        
+        // Compute the new image size that preserves aspect ratio
+        let scaledImageSize = CGSize(
+          width: size.width * scaleFactor,
+          height: size.height * scaleFactor
+        )
+        
+        // Draw and return the resized UIImage
+        let renderer = UIGraphicsImageRenderer(
+          size: scaledImageSize
+        )
+        
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+              origin: .zero,
+              size: scaledImageSize
+            ))
         }
-
-        // This is the rect that we've calculated out and this is what is actually used below
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-
-        // Actually do the resizing to the rect using the ImageContext stuff
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        self.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        return newImage!
+        
+        return scaledImage
     }
 }
